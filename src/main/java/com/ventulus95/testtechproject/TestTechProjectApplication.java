b0538fba606ab317d0e78cab3ae39dabc62555a6
이창섭 <ventulus95@gmail.com>
2024-01-20 19:29:53 +0900

    ADD: init commit

---
+++ src/main/java/com/ventulus95/testtechproject/TestTechProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ventulus95.testtechproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTechProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventulus95/testtechproject/TestTechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventulus95.testtechproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
